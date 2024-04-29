64d62858f81cf93af3a658d68543b2e6cf739597
Pablo Orviz <orviz@ifca.unican.es>
2024-04-29 12:47:34 +0200

    First version of setup.py

---
+++ setup.py
@@ -0,0 +1,41 @@
+# SPDX-FileCopyrightText: Copyright contributors to the Software Quality Assurance as a Service (SQAaaS) project <sqaaas@ibergrid.eu>
+# SPDX-FileContributor: Pablo Orviz <orviz@ifca.unican.es>
+#
+# SPDX-License-Identifier: GPL-3.0-only
+
+# coding: utf-8
+
+from setuptools import find_packages, setup
+
+NAME = "sqaaas_api_utils"
+VERSION = "1.0.0"
+
+# To install the library, run the following
+#
+# python setup.py install
+#
+# prerequisite: setuptools
+# http://pypi.python.org/pypi/setuptools
+
+REQUIRES = [
+]
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description="SQAaaS API Utils",
+    author="Pablo Orviz",
+    author_email="orviz@ifca.unican.es",
+    url="https://github.com/eosc-synergy/sqaaas-api-utils",
+    keywords=["SQAaaS API"],
+    install_requires=REQUIRES,
+    packages=find_packages(),
+    include_package_data=False,
+    long_description="""\
+    Frequently used utils to programmatically interact with the SQAaaS API.
+    """,
+    setuptools_git_versioning={
+        "enabled": True,
+    },
+    setup_requires=["setuptools-git-versioning>=2.0,<3"],
+)
