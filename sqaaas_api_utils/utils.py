e3200ef335c5e9b2dec9a634ba0204fd1b144029
Pablo Orviz <orviz@ifca.unican.es>
2024-05-13 11:43:14 +0200

    Assessment method!

--- sqaaas_api_utils/utils.py
+++ sqaaas_api_utils/utils.py
@@ -27,3 +27,46 @@ def get_tool_data(tool, lang):
     data = req.json()
 
     return data["tools"][lang][tool]
+
+
+def run_assessment(repo, branch=None, step_tools=[]):
+    pipeline_id = None
+    action = "create"
+    sqaaas_report_json = {}
+
+    wait_period = 5
+    keep_trying = True
+    while keep_trying:
+        logger.info(f"Performing {action} on pipeline {pipeline_id}")
+        if action in ["create"]:
+            payload = json.loads(create_payload(repo, branch, step_tools))
+            logging.debug("Using payload: %s" % payload)
+            response = sqaaas_request("post", "pipeline/assessment", payload=payload)
+            response_data = response.json()
+            pipeline_id = response_data["id"]
+            action = "run"
+        elif action in ["run"]:
+            response = sqaaas_request("post", f"pipeline/{pipeline_id}/{action}")
+            action = "status"
+        elif action in ["status"]:
+            response = sqaaas_request("get", f"pipeline/{pipeline_id}/{action}")
+            response_data = response.json()
+            build_status = response_data["build_status"]
+            if build_status in COMPLETED_STATUS:
+                action = "output"
+                logging.info(
+                    f"Pipeline {pipeline_id} finished with status {build_status}"
+                )
+            else:
+                time.sleep(wait_period)
+                logger.info(
+                    f"Current status is {build_status}. Waiting {wait_period} seconds.."
+                )
+        elif action in ["output"]:
+            keep_trying = False
+            response = sqaaas_request(
+                "get", f"pipeline/assessment/{pipeline_id}/{action}"
+            )
+            sqaaas_report_json = response.json()
+
+    return sqaaas_report_json
